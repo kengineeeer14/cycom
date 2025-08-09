f2be39daf180dd244a1ee9bbb72f319482958afe
ken <kengineeeer14@gmail.com>
2025-08-09 17:15:31 +0900

    テスト用に修正

--- src/gpio/DEV_Config.cc
+++ src/gpio/DEV_Config.cc
@@ -53,13 +53,6 @@ void UartConfig::DevSetGpioMode(const UWORD &pin, const UWORD &mode) {
 }
 
 UBYTE UartConfig::DevModuleInit() {
-    if (wiringPiSetupGpio() < 0) {
-        std::cerr << "set wiringPi lib failed !!!" << std::endl;
-        return 1;
-    } else {
-        std::cout << "set wiringPi lib success !!!" << std::endl;
-    }
-
     // fd = serialOpen(uart_port.c_str(), 9600);   // TODO: マジックナンバーの使用を避ける．
     // if (fd < 0) {
     //     return 1;
