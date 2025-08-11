777e3f197e34a8cc8c038ff03e898b8517564305
ken <kengineeeer14@gmail.com>
2025-08-11 16:11:40 +0900

    main.ccをリファクタ

--- main.cc
+++ main.cc
@@ -7,19 +7,48 @@
 #include "sensor/uart/uart_config.h"
 #include "sensor/uart/gps/gps_l76k.h"
 
+void process_nmea_line(sensor_uart::L76k &gps, const std::string &line) {
+    std::string nmea_line = line;
+    if (!nmea_line.empty() && nmea_line.back() == '\r') {
+        nmea_line.pop_back(); // \r 削除
+    }
+
+    if (nmea_line.rfind("$GNRMC", 0) == 0) {
+        sensor_uart::L76k::GNRMC rmc{};
+        if (gps.parseGNRMC(nmea_line, rmc)) {
+            std::cout << "[GNRMC] Lat: " << rmc.latitude << rmc.lat_dir
+                      << " Lon: " << rmc.longitude << rmc.lon_dir
+                      << " Speed(knots): " << rmc.speed_knots
+                      << " Track: " << rmc.track_deg << "\n";
+        }
+    } else if (nmea_line.rfind("$GNGGA", 0) == 0) {
+        sensor_uart::L76k::GNGGA gga{};
+        if (gps.parseGNGGA(nmea_line, gga)) {
+            std::cout << "[GNGGA] quality: " << static_cast<int>(gga.quality)
+                      << " num_satellites: " << static_cast<int>(gga.num_satellites)
+                      << " dgps_id: " << gga.dgps_id << "\n";
+        }
+    } else if (nmea_line.rfind("$GNVTG", 0) == 0) {
+        sensor_uart::L76k::GNVTG vtg{};
+        if (gps.parseGNVTG(nmea_line, vtg)) {
+            std::cout << "[GNVTG] True Track: " << vtg.true_track_deg << vtg.true_track_indicator
+                      << " Speed(knots): " << vtg.speed_knots
+                      << " Speed(km/h): " << vtg.speed_kmh << "\n";
+        }
+    }
+}
+
 int main() {
     const std::string config_path = "config/config.json";
 
     gpio::GpioConfigure gpio_config;
     sensor_uart::UartConfigure uart_config(config_path);
-    sensor_uart::L76k gps;   // インスタンス作成
+    sensor_uart::L76k gps;
 
-    // GPIOの設定
     if (!gpio_config.SetupGpio()) {
         return 1;
     }
 
-    // UARTの設定
     int fd = uart_config.SetupUart();
     if (fd < 0) {
         return 1;
@@ -29,39 +58,16 @@ int main() {
     std::string nmea_line;
 
     while (true) {
-        int n = read(fd, buf, sizeof(buf) - 1); // 読み取ったバイト数を取得
+        int n = read(fd, buf, sizeof(buf) - 1);
         if (n > 0) {
-            buf[n] = '\0';  // 文字列の終わりを判別できるようにヌル終端を追加
-            for (int i = 0; i < n; i++) {
-                char c = buf[i];
-                if (c == '\n') {
-                    // NMEA 1行が揃ったら処理
-                    if (!nmea_line.empty() && nmea_line.back() == '\r') {
-                        nmea_line.pop_back(); // \r削除
-                    }
+            buf[n] = '\0';
+            nmea_line.append(buf, n);
 
-                    // --- パーサ呼び出し ---
-                    if (nmea_line.rfind("$GNRMC", 0) == 0) {
-                        sensor_uart::L76k::GNRMC rmc{};
-                        if (gps.parseGNRMC(nmea_line, rmc)) {
-                            std::cout << "[GNRMC] Lat: " << rmc.latitude << rmc.lat_dir
-                                      << " Lon: " << rmc.longitude << rmc.lon_dir
-                                      << " Speed(knots): " << rmc.speed_knots
-                                      << " Track: " << rmc.track_deg << "\n";
-                        }
-                    } else if (nmea_line.rfind("$GNGGA", 0) == 0) {
-                        sensor_uart::L76k::GNGGA gga{};
-                        if (gps.parseGNGGA(nmea_line, gga)) {
-                            std::cout << "[GNGGA] quality: " << gga.quality
-                                      << " num_satellites: " << gga.num_satellites
-                                      << " dgps_id: " << gga.dgps_id << "\n";
-                        }
-                    }
-                    nmea_line.clear(); // 次の行へ
-                }
-                else {
-                    nmea_line.push_back(c);
-                }
+            size_t pos;
+            while ((pos = nmea_line.find('\n')) != std::string::npos) {
+                std::string line = nmea_line.substr(0, pos + 1);
+                process_nmea_line(gps, line);
+                nmea_line.erase(0, pos + 1);
             }
         }
     }
