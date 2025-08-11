50bed30b6e76cfcd189b26dd3b6fab963f2f4451
ken <kengineeeer14@gmail.com>
2025-08-11 23:23:28 +0900

    ガード修正

--- src/sensor/uart/gps/gps_l76k.cc
+++ src/sensor/uart/gps/gps_l76k.cc
@@ -1,3 +1,4 @@
+#include <limits>
 #include "sensor/uart/gps/gps_l76k.h"
 
 namespace sensor_uart{
@@ -13,8 +14,6 @@ namespace sensor_uart{
     }
 
     void L76k::ParseGnrmc(const std::string &nmea, GNRMC &gnrmc) {
-        // TODO マジックナンバーの使用を回避
-
         std::vector<std::string> fields = SplitString(nmea);
 
         // UTC時刻
@@ -23,25 +22,24 @@ namespace sensor_uart{
             gnrmc.minute = std::stoi(fields[1].substr(2, 2));
             gnrmc.second = std::stod(fields[1].substr(4));
         } else {
-            gnrmc.hour = 0;
-            gnrmc.minute = 0;
-            gnrmc.second = 0.0;
+            gnrmc.hour = UINT8_MAX;
+            gnrmc.minute = UINT8_MAX;
+            gnrmc.second = std::numeric_limits<double>::quiet_NaN();
         }
-        gnrmc.data_status = (fields.size() > 2 && !fields[2].empty()) ? fields[2][0] : ' ';
-        gnrmc.latitude    = (fields.size() > 3 && !fields[3].empty()) ? std::stod(fields[3]) : 0.0;
-        gnrmc.lat_dir     = (fields.size() > 4 && !fields[4].empty()) ? fields[4][0] : ' ';
-        gnrmc.longitude   = (fields.size() > 5 && !fields[5].empty()) ? std::stod(fields[5]) : 0.0;
-        gnrmc.lon_dir     = (fields.size() > 6 && !fields[6].empty()) ? fields[6][0] : ' ';
-        gnrmc.speed_knots = (fields.size() > 7 && !fields[7].empty()) ? std::stod(fields[7]) : 0.0;
-        gnrmc.track_deg   = (fields.size() > 8 && !fields[8].empty()) ? std::stod(fields[8]) : 0.0;
-        gnrmc.date        = (fields.size() > 9 && !fields[9].empty()) ? static_cast<uint16_t>(std::stoi(fields[9])) : 0;
-        gnrmc.mag_variation = (fields.size() > 10 && !fields[10].empty()) ? std::stod(fields[10]) : 0.0;
-        gnrmc.mag_variation_dir = (fields.size() > 11 && !fields[11].empty()) ? fields[11][0] : ' ';
-        gnrmc.mode = (fields.size() > 12 && !fields[12].empty()) ? fields[12][0] : ' ';
-
+        gnrmc.data_status = (fields.size() > 2 && !fields[2].empty()) ? fields[2][0] : '\0';
+        gnrmc.latitude    = (fields.size() > 3 && !fields[3].empty()) ? std::stod(fields[3]) : std::numeric_limits<double>::quiet_NaN();
+        gnrmc.lat_dir     = (fields.size() > 4 && !fields[4].empty()) ? fields[4][0] : '\0';
+        gnrmc.longitude   = (fields.size() > 5 && !fields[5].empty()) ? std::stod(fields[5]) : std::numeric_limits<double>::quiet_NaN();
+        gnrmc.lon_dir     = (fields.size() > 6 && !fields[6].empty()) ? fields[6][0] : '\0';
+        gnrmc.speed_knots = (fields.size() > 7 && !fields[7].empty()) ? std::stod(fields[7]) : std::numeric_limits<double>::quiet_NaN();
+        gnrmc.track_deg   = (fields.size() > 8 && !fields[8].empty()) ? std::stod(fields[8]) : std::numeric_limits<double>::quiet_NaN();
+        gnrmc.date        = (fields.size() > 9 && !fields[9].empty()) ? static_cast<uint16_t>(std::stoi(fields[9])) : UINT16_MAX;
+        gnrmc.mag_variation = (fields.size() > 10 && !fields[10].empty()) ? std::stod(fields[10]) : std::numeric_limits<double>::quiet_NaN();
+        gnrmc.mag_variation_dir = (fields.size() > 11 && !fields[11].empty()) ? fields[11][0] : '\0';
+        gnrmc.mode = (fields.size() > 12 && !fields[12].empty()) ? fields[12][0] : '\0';
 
         // navigation_status と checksum（<nav>*CS のみ扱う）
-        gnrmc.navigation_status = (fields.size() > 13 && !fields[13].empty()) ? fields[13][0] : ' ';
+        gnrmc.navigation_status = (fields.size() > 13 && !fields[13].empty()) ? fields[13][0] : '\0';
         gnrmc.checksum = 0;  // デフォルトは0（無効扱い）
 
         if (fields.size() > 13) {
@@ -54,22 +52,20 @@ namespace sensor_uart{
     }
 
     void L76k::ParseGnvtg(const std::string &nmea, GNVTG &gnvtg) {
-        // TODO マジックナンバーの使用を回避
-
         std::vector<std::string> fields = SplitString(nmea);
 
-        gnvtg.true_track_deg           = (fields.size() > 1 && !fields[1].empty()) ? std::stod(fields[1]) : 0.0;
-        gnvtg.true_track_indicator     = (fields.size() > 2 && !fields[2].empty()) ? fields[2][0] : ' ';
-        gnvtg.magnetic_track_deg       = (fields.size() > 3 && !fields[3].empty()) ? std::stod(fields[3]) : 0.0;
-        gnvtg.magnetic_track_indicator = (fields.size() > 4 && !fields[4].empty()) ? fields[4][0] : ' ';
-        gnvtg.speed_knots              = (fields.size() > 5 && !fields[5].empty()) ? std::stod(fields[5]) : 0.0;
-        gnvtg.speed_knots_unit         = (fields.size() > 6 && !fields[6].empty()) ? fields[6][0] : ' ';
-        gnvtg.speed_kmh                = (fields.size() > 7 && !fields[7].empty()) ? std::stod(fields[7]) : 0.0;
+        gnvtg.true_track_deg           = (fields.size() > 1 && !fields[1].empty()) ? std::stod(fields[1]) : std::numeric_limits<double>::quiet_NaN();
+        gnvtg.true_track_indicator     = (fields.size() > 2 && !fields[2].empty()) ? fields[2][0] : '\0';
+        gnvtg.magnetic_track_deg       = (fields.size() > 3 && !fields[3].empty()) ? std::stod(fields[3]) : std::numeric_limits<double>::quiet_NaN();
+        gnvtg.magnetic_track_indicator = (fields.size() > 4 && !fields[4].empty()) ? fields[4][0] : '\0';
+        gnvtg.speed_knots              = (fields.size() > 5 && !fields[5].empty()) ? std::stod(fields[5]) : std::numeric_limits<double>::quiet_NaN();
+        gnvtg.speed_knots_unit         = (fields.size() > 6 && !fields[6].empty()) ? fields[6][0] : '\0';
+        gnvtg.speed_kmh                = (fields.size() > 7 && !fields[7].empty()) ? std::stod(fields[7]) : std::numeric_limits<double>::quiet_NaN();
 
         if (fields.size() > 8 && !fields[8].empty()) {
             size_t starPos = fields[8].find('*');
             gnvtg.speed_kmh_unit = fields[8][0];
-            gnvtg.mode = (starPos > 1) ? fields[8][1] : ' ';
+            gnvtg.mode = (starPos > 1) ? fields[8][1] : '\0';
             if (starPos != std::string::npos && starPos + 1 < fields[8].size()) {
                 gnvtg.checksum = std::stoi(fields[8].substr(starPos + 1), nullptr, 16);
             }
@@ -77,8 +73,6 @@ namespace sensor_uart{
     }
 
     void L76k::ParseGngaa(const std::string &nmea, GNGGA &gngaa) {
-        // TODO マジックナンバーの使用を回避
-
         std::vector<std::string> fields = SplitString(nmea);
 
         if (fields.size() > 1 && fields[1].size() >= 6) {
@@ -86,22 +80,22 @@ namespace sensor_uart{
             gngaa.minute = std::stoi(fields[1].substr(2, 2));
             gngaa.second = std::stod(fields[1].substr(4));
         } else {
-            gngaa.hour = 0;
-            gngaa.minute = 0;
-            gngaa.second = 0.0;
+            gngaa.hour = UINT8_MAX;
+            gngaa.minute = UINT8_MAX;
+            gngaa.second = std::numeric_limits<double>::quiet_NaN();
         }
-        gngaa.latitude      = (fields.size() > 2 && !fields[2].empty()) ? std::stod(fields[2]) : 0.0;
-        gngaa.lat_dir       = (fields.size() > 3 && !fields[3].empty()) ? fields[3][0] : ' ';
-        gngaa.longitude     = (fields.size() > 4 && !fields[4].empty()) ? std::stod(fields[4]) : 0.0;
-        gngaa.lon_dir       = (fields.size() > 5 && !fields[5].empty()) ? fields[5][0] : ' ';
-        gngaa.quality       = (fields.size() > 6 && !fields[6].empty()) ? static_cast<uint8_t>(std::stoi(fields[6])) : 0;
-        gngaa.num_satellites= (fields.size() > 7 && !fields[7].empty()) ? static_cast<uint8_t>(std::stoi(fields[7])) : 0;
-        gngaa.hdop          = (fields.size() > 8 && !fields[8].empty()) ? std::stod(fields[8]) : 0.0;
-        gngaa.altitude      = (fields.size() > 9 && !fields[9].empty()) ? std::stod(fields[9]) : 0.0;
-        gngaa.altitude_unit = (fields.size() > 10 && !fields[10].empty()) ? fields[10][0] : ' ';
-        gngaa.geoid_height  = (fields.size() > 11 && !fields[11].empty()) ? std::stod(fields[11]) : 0.0;
-        gngaa.geoid_unit    = (fields.size() > 12 && !fields[12].empty()) ? fields[12][0] : ' ';
-        gngaa.dgps_age      = (fields.size() > 13 && !fields[13].empty()) ? std::stod(fields[13]) : 0.0;
+        gngaa.latitude      = (fields.size() > 2 && !fields[2].empty()) ? std::stod(fields[2]) : std::numeric_limits<double>::quiet_NaN();
+        gngaa.lat_dir       = (fields.size() > 3 && !fields[3].empty()) ? fields[3][0] : '\0';
+        gngaa.longitude     = (fields.size() > 4 && !fields[4].empty()) ? std::stod(fields[4]) : std::numeric_limits<double>::quiet_NaN();
+        gngaa.lon_dir       = (fields.size() > 5 && !fields[5].empty()) ? fields[5][0] : '\0';
+        gngaa.quality       = (fields.size() > 6 && !fields[6].empty()) ? static_cast<uint8_t>(std::stoi(fields[6])) : UINT8_MAX;
+        gngaa.num_satellites= (fields.size() > 7 && !fields[7].empty()) ? static_cast<uint8_t>(std::stoi(fields[7])) : UINT8_MAX;
+        gngaa.hdop          = (fields.size() > 8 && !fields[8].empty()) ? std::stod(fields[8]) : std::numeric_limits<double>::quiet_NaN();
+        gngaa.altitude      = (fields.size() > 9 && !fields[9].empty()) ? std::stod(fields[9]) : std::numeric_limits<double>::quiet_NaN();
+        gngaa.altitude_unit = (fields.size() > 10 && !fields[10].empty()) ? fields[10][0] : '\0';
+        gngaa.geoid_height  = (fields.size() > 11 && !fields[11].empty()) ? std::stod(fields[11]) : std::numeric_limits<double>::quiet_NaN();
+        gngaa.geoid_unit    = (fields.size() > 12 && !fields[12].empty()) ? fields[12][0] : '\0';
+        gngaa.dgps_age      = (fields.size() > 13 && !fields[13].empty()) ? std::stod(fields[13]) : std::numeric_limits<double>::quiet_NaN();
 
         if (fields.size() > 14 && !fields[14].empty()) {
             size_t starPos = fields[14].find('*');
